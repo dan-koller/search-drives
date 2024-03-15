2eff3ea336dfaece357d126de4a4a7f5a32405ef
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2024-03-15 10:14:50 +0100

    Added a python script for searching cross-platform

--- README.md
+++ README.md
@@ -1,39 +1,55 @@
 # Search Drives
 
-This is a collection of scripts to search for files on a drive or in a directory. This includes searching for files by name or patterns like `*.txt` or `*.jpg`. The results are saved to a text file including the full path to the file. You can then choose to copy all results to a new directory. No external libraries, installation or admin rights are required.
+This is a collection of scripts to search for files on a drive or in a directory. This includes searching for files by name or patterns like `*.txt` or `*.jpg`. The results are saved to a text file including the full path to the file. You can then choose to copy all results to a new directory. No external libraries, installation or admin rights\* are required.
 
 This software is provided as-is and for forensic purposes only. Use at your own risk.
 
+_\* To access certain directories, you may need admin rights depending on your operating system and the directory you want to search._
+
 ## Usage
 
-1. Download the script for your operating system
+I recommend to use the Python script, as it is the most flexible and easiest to use. However, if you don't have Python installed, you can use the batch or bash script.
 
-    ### Windows
+### Python
 
-    [search-win.bat](search-win.bat)
+> Requires Python 3.11 or higher. Check your version with `python --version`.
 
-    ### macOS & Linux\*
-
-    [search-bash.sh](search-bash.sh)
+1. Download the Python script [search.py](search.py)
 
 2. Run the script from the command line
 
-    ### Windows
-
-    ```bat
-    search-win.bat
-    ```
-
-    ### macOS & Linux\*
-
-    _Requires Bash 3.2 or higher. Check your version with `bash --version`._
-
     ```bash
-    chmod +x search-bash.sh
-    ./search-bash.sh
+    python search.py
     ```
 
-    _\*) The script was tested on macOS, Ubuntu, and WSL on Windows 10. However, there may be differences when using default directories like `C:\Users\` on Windows, `/home/` on Linux, or `/Users/` on macOS. If you encounter any issues, please open an issue on GitHub._
+3. Follow the instructions on the screen
+
+### Batch & Bash
+
+1. Download the script for your operating system
+
+    - Windows: [search-win.bat](shell-scripts/search-win.bat)
+
+    - macOS & Linux\*: [search-bash.sh](shell-scripts/search-bash.sh)
+
+2. Run the script from the command line
+
+    - Windows
+
+        ```bat
+        search-win.bat
+        ```
+
+    - macOS & Linux\*
+
+        _Requires Bash 3.2 or higher. Check your version with `bash --version`._
+
+        ```bash
+        chmod +x search-bash.sh
+        ./search-bash.sh
+        ```
+
+        _\*) The script was tested on macOS, Ubuntu, and WSL on Windows 10. However, there may be differences when using default directories like `C:\Users\` on Windows, `/home/` on Linux, or `/Users/` on macOS. If you encounter any issues, please open an issue on GitHub._
 
 3. Follow the instructions on the screen
 
---
+++ search.py
@@ -0,0 +1,122 @@
+#############################################
+# This script searches for files and/or paths
+# on any system using Python. Select a volume 
+# or a path and specify the file or pattern
+# to search for. For forensic purposes only.
+# Use at your own risk.
+#
+# Author: Dan Koller
+# Date: 14.03.2024
+# Version: 1.0
+# License: MIT
+#############################################
+
+import os
+import shutil
+from datetime import datetime
+import fnmatch
+
+def main():
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    results_file = f"results-{timestamp}.txt"
+
+    while True:
+        print("Select an option:")
+        print("1. Search for a drive")
+        print("2. Search for a path")
+        print("3. Exit")
+        option = input("Enter an option: ")
+
+        if option == "1":
+            selected_drive = select_drive()
+            if selected_drive:
+                search(selected_drive, results_file)
+                copy_option(results_file)
+        elif option == "2":
+            selected_path = select_path()
+            if selected_path:
+                search(selected_path, results_file)
+                copy_option(results_file)
+        elif option == "3":
+            break
+        else:
+            print("Invalid option. Please select a valid option.")
+
+def select_path():
+    while True:
+        selected_path = input("Enter the path: ")
+        if os.path.isdir(selected_path):
+            print(f"Selected path: {selected_path}")
+            return selected_path
+        else:
+            print("Invalid path. Please provide a valid path like C:\\Users\\ or D:\\.")
+
+def select_drive():
+    print("Listing Available Drives:")
+    print("-------------------------")
+    drives = [drive.strip() for drive in os.popen("wmic logicaldisk get caption").read().split()[1:]]
+    print("\n".join(drives))
+
+    while True:
+        selected_drive = input("Enter a drive letter (e.g., C:, D:, E:) or press enter to set a path: ").upper()
+        if selected_drive in drives or os.path.isdir(selected_drive):
+            print(f"Selected drive: {selected_drive}")
+            return selected_drive
+        else:
+            print("Invalid drive letter or path. Please select a valid drive like C:, D:, E: or provide a valid path.")
+
+def search(target, results_file):
+    search_terms_input = input("Enter the search pattern (e.g., *.exe, *.py, enter for all files): ").strip()
+    search_terms = search_terms_input.split()
+    if not search_terms:
+        search_terms = ["*"]
+
+    print(f"Searching for: {', '.join(search_terms)}")
+    with open(results_file, "w") as f:
+        for root, dirs, files in os.walk(target):
+            for file in files:
+                for term in search_terms:
+                    if fnmatch.fnmatch(file, term):
+                        result_path = os.path.join(root, file)
+                        f.write(result_path + "\n")
+
+    count = sum(1 for line in open(results_file))
+    print(f"Found {count} results in {target}.")
+    os.startfile(results_file)
+
+def copy_option(results_file):
+    if os.path.getsize(results_file) == 0:
+        return
+
+    copy_results = input("Do you want to copy the results to a folder? (Y/N): ").upper()
+    if copy_results == "Y":
+        copy_results_to_folder(results_file)
+    elif copy_results == "N":
+        pass
+    else:
+        print("Invalid input. Please enter Y or N.")
+
+def copy_results_to_folder(results_file):
+    while True:
+        copy_path = input("Enter the path to copy the results to: ")
+        if os.path.isdir(copy_path):
+            with open(results_file) as f:
+                for line in f:
+                    src_file = line.strip()
+                    dest_file = os.path.join(copy_path, os.path.basename(src_file))
+                    if os.path.exists(dest_file):
+                        overwrite = input(f"File {os.path.basename(src_file)} already exists. Overwrite? (Y/N): ").upper()
+                        if overwrite != "Y":
+                            continue
+                    try:
+                        shutil.copy(src_file, copy_path)
+                    except Exception as e:
+                        print(f"Error copying file: {e}")
+                    else:
+                        print(f"File {os.path.basename(src_file)} copied to {copy_path}.")
+            break
+        else:
+            print("Invalid path. Please provide a valid path like C:\\Users\\ or D:\\.")
+
+if __name__ == "__main__":
+    main()
